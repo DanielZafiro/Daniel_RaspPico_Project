c342502db2251eb4101945a424f57957ac725026
DanieLudens <66543657+DanielZafiro@users.noreply.github.com>
2024-03-07 04:02:57 -0500

    Update codeTerceraManera.ino the code of third way of solving the given task

--- Ejercicio 14/codeTerceraManera.ino
+++ Ejercicio 14/codeTerceraManera.ino
@@ -1,16 +1,64 @@
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+static void interchangeVar(uint32_t &a, uint32_t &b)
+{
+    uint32_t temp = a;
+    a = b;
+    b = temp;
 }
 
-void loop() {
-  static uint32_t previousTime = 0;
-  static bool ledState = true;
+static void printInterchange(uint32_t x, uint32_t y)
+{
+    Serial.print("Intercambio: x = ");
+    Serial.print(x);
+    Serial.print(", y = ");
+    Serial.println(y);
+    Serial.print('\n');
+}
 
-  uint32_t currentTime = millis();
+void task1()
+{
+    enum class Task1States    {
+        INIT,
+        WAIT_DATA
+    };
+    static Task1States task1State = Task1States::INIT;
 
-  if( (currentTime - previousTime) > 1000){
-    previousTime = currentTime;
-    ledState = !ledState;
-    digitalWrite(LED_BUILTIN, ledState);
-  }
+    switch (task1State)
+    {
+    case Task1States::INIT:
+    {
+        Serial.begin(115200);
+        task1State = Task1States::WAIT_DATA;
+        break;
+    }
+
+    case Task1States::WAIT_DATA:
+    {
+        // evento 1:        // Ha llegado al menos un dato por el puerto serial?        
+  if (Serial.available() > 0)
+        {
+            Serial.read();
+            uint32_t x = 5;
+            uint32_t y = 25;
+            printInterchange(x, y);
+            interchangeVar(x, y);
+            printInterchange(x, y);
+        }
+        break;
+    }
+
+    default:
+    {
+        break;
+    }
+    }
+}
+
+void setup()
+{
+    task1();
+}
+
+void loop()
+{
+    task1();
 }
