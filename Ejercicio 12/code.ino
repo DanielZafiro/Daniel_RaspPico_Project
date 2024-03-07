147dfc7b91b64758065e2146e744d9cd50b6ef3a
DanieLudens <66543657+DanielZafiro@users.noreply.github.com>
2024-03-07 01:43:55 -0500

    Update code.ino of the funtion with pointers and explanation

--- Ejercicio 12/code.ino
+++ Ejercicio 12/code.ino
@@ -1,16 +1,59 @@
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+void task1()
+{
+    enum class Task1States    {
+        INIT,
+        WAIT_DATA
+    };
+    static Task1States task1State = Task1States::INIT;
+
+    switch (task1State)
+    {
+    case Task1States::INIT:
+    {
+        Serial.begin(115200);
+        task1State = Task1States::WAIT_DATA;
+        break;
+    }
+
+    case Task1States::WAIT_DATA:
+    {
+        // evento 1:        // Ha llegado al menos un dato por el puerto serial? 
+       if (Serial.available() > 0)
+        {
+            // DEBES leer ese dato, sino se acumula y el buffer de recepción del serial se llenará.   
+            Serial.read();
+
+            uint32_t var = 0; // Declara la variable var inicializadad en 0
+	    uint32_t *pvar = &var; // Declara un puntero pvar que apunta a la direccion var y Almacena en pvar la dirección de var.
+
+            // Envía por el serial el contenido de var usando el apuntador pvar. 
+            Serial.print("var content: ");
+            Serial.print(*pvar);
+            Serial.print('\n');
+
+            *pvar = 10; // Escribir un nuevo valor en pvar mediante el puntero
+
+	    // Envía por el serial el contenido actualizado de var usando el apuntador pvar. 
+            Serial.print("var content: ");
+            Serial.print(*pvar);
+            Serial.print('\n');
+        }
+        break;
+    }
+
+    default:
+    {
+        break;
+    }
+    }
 }
 
-void loop() {
-  static uint32_t previousTime = 0;
-  static bool ledState = true;
+void setup()
+{
+    task1();
+}
 
-  uint32_t currentTime = millis();
-
-  if( (currentTime - previousTime) > 1000){
-    previousTime = currentTime;
-    ledState = !ledState;
-    digitalWrite(LED_BUILTIN, ledState);
-  }
+void loop()
+{
+    task1();
 }
