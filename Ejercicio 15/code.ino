50245f977a9a217f2125b2baf8e5822be79e7453
DanieLudens <66543657+DanielZafiro@users.noreply.github.com>
2024-03-07 06:11:22 -0500

    Update code.ino with the code fixed

--- Ejercicio 15/code.ino
+++ Ejercicio 15/code.ino
@@ -1,16 +1,67 @@
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+static void processData(uint8_t *pData, uint8_t size, uint8_t *res)
+{
+  uint8_t sum = 0;
+  for(int i = 0; i < size; i++)
+    {
+     sum = sum + (pData[i] - 0x30);
+    }
+  *res = sum;
 }
 
-void loop() {
-  static uint32_t previousTime = 0;
-  static bool ledState = true;
+enum class Task1States
+{
+  INIT,
+  WAIT_DATA
+};
 
-  uint32_t currentTime = millis();
+static Task1States task1State = Task1States::INIT;
+static uint8_t rxData[5];
+static uint8_t dataCounter = 0;
 
-  if( (currentTime - previousTime) > 1000){
-    previousTime = currentTime;
-    ledState = !ledState;
-    digitalWrite(LED_BUILTIN, ledState);
-  }
+void task1()
+{
+  switch(task1State)
+    {
+    case Task1States::INIT:
+        {
+      Serial.begin(115200);
+      task1State = Task1States::WAIT_DATA;
+      break;
+        }
+    
+    case Task1States::WAIT_DATA:
+    {
+      // Evento 1:
+
+      if(Serial.available() > 0)
+        {
+        rxData[dataCounter] = Serial.read();
+        dataCounter++;
+        if(dataCounter == 5)
+            {
+              uint8_t result = 0;
+              processData(rxData, dataCounter, &result);
+              dataCounter = 0;
+              Serial.print("result: ");
+              Serial.print(result);
+              Serial.print('\n');
+            }
+        }
+      break;
+    }
+      default:
+            {
+              break;
+            }
+      }
+}
+
+void setup()
+{
+  task1();
+}
+
+void loop()
+{
+  task1();
 }
