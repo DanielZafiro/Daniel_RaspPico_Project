2c0e45c0280abedd3550b65ecb5a9846d36e7dbc
DanieLudens <66543657+DanielZafiro@users.noreply.github.com>
2024-03-06 21:08:07 -0500

    Update code.ino with the code that works for the exercise

--- Ejercicio 9/code.ino
+++ Ejercicio 9/code.ino
@@ -1,16 +1,119 @@
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-}
+    void task1(){
+        enum class Task1States{
+            INIT,
+            WAIT_FOR_TIMEOUT
+        };
+    
+        static Task1States task1State = Task1States::INIT;
+        static uint32_t lastTime;
+        static constexpr uint32_t INTERVAL = 1000;
+    
+        switch(task1State){
+            case Task1States::INIT:{
+                Serial.begin(115200);
+                lastTime = millis();
+                task1State = Task1States::WAIT_FOR_TIMEOUT;
+                break;
+            }
+    
+            case Task1States::WAIT_FOR_TIMEOUT:{
+                // evento 1:            
+                uint32_t currentTime = millis();
+                if( (currentTime - lastTime) >= INTERVAL ){
+                    lastTime = currentTime;
+                    Serial.print("mensaje a 1Hz\n");
+                }
+                break;
+            }
+    
+            default:{
+                break;
+            }
+        }
+    
+    }
 
-void loop() {
-  static uint32_t previousTime = 0;
-  static bool ledState = true;
+    void task2(){
+        enum class Task2States{
+            INIT,
+            WAIT_FOR_TIMEOUT
+        };
+    
+        static Task2States task2State = Task2States::INIT;
+        static uint32_t lastTime;
+        static constexpr uint32_t INTERVAL = 500;
+    
+        switch(task2State){
+            case Task2States::INIT:{
+                Serial.begin(115200);
+                lastTime = millis();
+                task2State = Task2States::WAIT_FOR_TIMEOUT;
+                break;
+            }
+    
+            case Task2States::WAIT_FOR_TIMEOUT:{
+                // evento 2:            
+                uint32_t currentTime = millis();
+                if( (currentTime - lastTime) >= INTERVAL ){
+                    lastTime = currentTime;
+                    Serial.print("mensaje a 0.5Hz\n");
+                }
+                break;
+            }
+    
+            default:{
+                break;
+            }
+        }
+    
+    }
 
-  uint32_t currentTime = millis();
-
-  if( (currentTime - previousTime) > 1000){
-    previousTime = currentTime;
-    ledState = !ledState;
-    digitalWrite(LED_BUILTIN, ledState);
-  }
-}
+     void task3(){
+        enum class Task3States{
+            INIT,
+            WAIT_FOR_TIMEOUT
+        };
+    
+        static Task3States task3State = Task3States::INIT;
+        static uint32_t lastTime;
+        static constexpr uint32_t INTERVAL = 250;
+    
+        switch(task3State){
+            case Task3States::INIT:{
+                Serial.begin(115200);
+                lastTime = millis();
+                task3State = Task3States::WAIT_FOR_TIMEOUT;
+                break;
+            }
+    
+            case Task3States::WAIT_FOR_TIMEOUT:{
+                // evento 3:            
+                uint32_t currentTime = millis();
+                if( (currentTime - lastTime) >= INTERVAL ){
+                    lastTime = currentTime;
+                    Serial.print("mensaje a 0.25Hz\n");
+                }
+                break;
+            }
+    
+            default:{
+                break;
+            }
+        }
+    
+    }
+    
+    void setup() // Inicializa las 3 tareas al principio del programa
+    {
+        task1();
+        task2();
+        task3();
+        
+    }
+    
+    void loop() // Hace que las tareas se realizen en bucle
+    {
+        task1();
+        task2();
+        task3();
+    }
